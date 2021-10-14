de6f69750e78754032e1b41595765b926cc9a928
shivangipatodiya <shivu2610@gmail.com>
2021-10-14 19:18:44 +0000

    add parametrised values

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,14 +6,16 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
-pool.query(`SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+const queryString = `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers 
      JOIN assistance_requests ON teachers.id = teacher_id
      JOIN students ON student_id = students.id
      JOIN cohorts ON cohorts.id = cohort_id   
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teachers.name;`)
+WHERE cohorts.name = $1
+ORDER BY teachers.name;`;
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`%${cohortName}%`]
+pool.query(queryString, values)
 .then((res) => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`)
